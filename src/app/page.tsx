a750ac392df90606f0653af8258d09b407093d07
kimddddh98 <amawang11@gmail.com>
2026-01-17 02:43:38 +0900

    ui: 헤더,메인페이지 마크업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,61 @@
+const products = Array.from({ length: 8 }).map((_, i) => ({
+  index: i + 1,
+  name: `음식명`,
+  price: 9999,
+  current: 11,
+  limit: 11,
+  image: '/image-test.png',
+}))
+
 export default function Home() {
-  return <div>hello world</div>
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <main className="mx-auto flex max-w-6xl flex-col gap-8 px-4 py-8">
+        <section className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-3xl font-bold text-gray-900">척척 밀키트</h1>
+            <p className="text-xl text-gray-500">
+              오늘 뭐 먹을지 고민된다면 여기서 골라보세요
+            </p>
+          </div>
+        </section>
+
+        <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+          {products.map((product) => (
+            <article
+              key={product.index}
+              className="border-ccbs-gray-200 overflow-hidden rounded-xl border bg-white shadow-sm transition hover:-translate-y-2 hover:shadow-md"
+            >
+              <div
+                className={`relative h-60 w-full bg-[url(${product.image})] bg-cover sm:h-50`}
+              >
+                {product.current > 0 && (
+                  <span className="absolute top-2 right-2 rounded-full bg-red-600 px-2 py-0.5 text-sm font-semibold text-white">
+                    인기
+                  </span>
+                )}
+              </div>
+
+              <div className="flex flex-col justify-between gap-2 p-4">
+                <div>
+                  <h2 className="text-ccbs-gray-800 font-semibold">
+                    {product.name}
+                  </h2>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <span className="text-ccbs-gray-800 text-lg font-bold">
+                    {product.price.toLocaleString()}원
+                  </span>
+                  <button className="bg-ccbs-green rounded-sm px-2 py-1.5 text-sm font-medium text-white hover:bg-[#53c0a7]">
+                    주문하기
+                  </button>
+                </div>
+              </div>
+            </article>
+          ))}
+        </section>
+      </main>
+    </div>
+  )
 }
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-10 border-b border-b-black/10 bg-white shadow-sm">
+      <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
+        <span className="text-cc text-ccbs-green text-xl font-bold">
+          척척밥상
+        </span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
